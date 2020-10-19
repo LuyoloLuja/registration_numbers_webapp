4c06d8d67010f1da5afa703444bbeac3a796c39b
LuyoloLuja <luyolom19@gmail.com>
2020-10-19 15:50:12 +0200

    initial commit

---
+++ factory-function/reg-numbers.js
@@ -0,0 +1,53 @@
+module.exports = function RegNumbers() {
+    var registrationNumbers = [];
+
+    function regSelection(selectReg) {
+        if (selectReg.startsWith("CA ") || selectReg.startsWith("CL ") || selectReg.startsWith("CJ ") || selectReg.startsWith("CAG ")) {
+            registrationNumbers.push(selectReg);
+        }
+    }
+
+    function appendRegNums(registrations) {
+
+        for (var i = 0; i < registrations.length; i++) {
+            let currentReg = registrations[i];
+            return currentReg;
+        }
+    }
+
+    function filter(regPlates) {
+        var regFilter = [];
+
+        for (var i = 0; i < registrationNumbers.length; i++) {
+            var currentNumberPlate = registrationNumbers[i]
+            if (currentNumberPlate.startsWith(regPlates)) {
+                regFilter.push(currentNumberPlate);
+            }
+        }
+        return regFilter;
+    }
+
+    function errorMessage(regNumbers){
+        registrationsEntered = [];
+
+        if (regNumbers !== startsWith("CA ") || regNumbers !== startsWith("CL ") || regNumbers !== startsWith("CJ ") || regNumbers !== startsWith("CAG ")) {
+            return "Please enter a valid registration number!";
+        }
+        if(regNumbers === "" || regNumbers === " "){
+            return "Please enter a registration number!";
+        }
+        if (registrationsEntered.inculdes(regNumbers)) {
+            return "Registration number already exists!";
+        }
+        if (regNumbers.length >= 10) {
+            return "Invalid! Registration should be less than 10!";
+        }
+    }
+
+    return {
+        regSelection,
+        appendRegNums,
+        filter,
+        errorMessage
+    }
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let app = express();
+const pg = require('pg');
+const Pool = pg.Pool;
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/reg_numbers';
+const pool = new Pool({
+	connectionString
+});
+
+const RegInstance = require('./factory-function/reg-numbers');
+const regInstance = RegInstance();
+
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.post('/registration', function(req, res){
+  
+})
+
+
+
+let PORT = process.env.PORT || 3031;
+
+app.listen(PORT, function(){
+  console.log('App starting on port', PORT);
+});
